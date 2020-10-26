e0f5a7df17fa44e61e8ed11768e14ea0d55bc67d
ClaireV38 <claireverdier00@gmail.com>
2020-10-26 11:26:43 +0100

    creation of OfferController and the view offer index

---
+++ src/Controller/OfferController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\OfferManager;
+
+/**
+ * Class OfferController
+ *
+ */
+class OfferController extends AbstractController
+{
+
+
+    /**
+     * Display item listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+     //   $offerManager = new OfferManager();
+      //  $offers = $offerManager->selectAll();
+
+        $offers = [0 => ['name' => "nomAnnonce1", 'category' => "nomCategorieA"], 1 => ['name' => "nomAnnonce1", 'category' => "nomCategorieB"], 2 => ['name' => "nomAnnonce1", 'category' => "nomCategorieA"]];
+
+        return $this->twig->render('Offer/index.html.twig', ['offers' => $offers]);
+    }
+
+}
