4b4ed33db61212544b7397ed25e9ef2da1bb88ca
ClaireV38 <claireverdier00@gmail.com>
2020-11-06 11:07:36 +0100

    add a new div container on home page to solve conflicts with results page

--- config/config.php
+++ config/config.php
@@ -9,16 +9,10 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
-
 define('APP_DEV', true);
 
 //Model (for connexion data, see unversionned db.php)
 //VIew
 define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
 define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
-
-<<<<<<< HEAD
-define('HOME_PAGE', 'offer/index');
-=======
 define('HOME_PAGE', 'Home/index');
->>>>>>> e17793cd1bd0600b2fa4f76b8070e7033b058857
