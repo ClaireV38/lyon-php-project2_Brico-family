de33818daa05aff2e598b895441e8dc5ce6d6eca
Grégory Troupenat <gregory.troupenat@gmail.com>
2020-11-11 11:46:38 +0100

    results() method revised

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -124,6 +124,87 @@ class OfferController extends AbstractController
      */
     public function results()
     {
-        return $this->twig->render('Offer/results.html.twig');
+        $productManager = new ProductManager();
+        $toolproducts = $productManager->selectByProductType('Tool');
+        $materialproducts = $productManager->selectByProductType('Material');
+
+        $products = [
+            'tools' => $toolproducts,
+            'materials' => $materialproducts
+        ];
+
+        $transactionManager = new TransactionManager();
+        $transactions = $transactionManager->selectAll();
+
+        $departmentManager = new DepartmentManager();
+        $departments = $departmentManager->selectAllOrderedByName();
+
+        $cityManager = new CityManager();
+        $citiesByDepartment = [];
+        foreach ($departments as $department) {
+            $citiesByDepartment[$department['name']] = $cityManager->selectCityByDepartement($department['name']);
+        }
+
+        $errors = [];
+        $productType = $product = $transaction = $department = $city = "";
+        $resultsOffer = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn-index-search']) && !empty($_POST)) {
+            if (!isset($_POST['tools_products']) && !isset($_POST['materials_products'])) {
+                $errors['product'] = 'Veuillez choisir une catégorie de produit';
+            }
+            if (!isset($_POST['product_type'])) {
+                $errors['productType'] = 'Veuillez choisir un type de produit';
+            } else {
+                $productType = $_POST['product_type'];
+                if (isset($_POST['tools_products']) && $productType === 'tool') {
+                    $product = $_POST['tools_products'];
+                } else {
+                    if (isset($_POST['materials_products'])) {
+                        $product = $_POST['materials_products'];
+                    }
+                }
+            }
+
+            if (!isset($_POST['transaction'])) {
+                $errors['transaction'] = 'Veuillez choisir un type de transaction';
+            } else {
+                $transaction = $_POST['transaction'];
+            }
+
+            if (!isset($_POST['city'])) {
+                $errors['city'] = 'Veuillez choisir une ville';
+            } else {
+                $city = $_POST['city'];
+            }
+
+            if (empty($errors)) {
+                $offerInfos = [
+                    'product' => $product,
+                    'productType' => $productType,
+                    'transaction' => $transaction,
+                    'department' => $department,
+                    'city' => $city
+                ];
+                $offerManager = new OfferManager();
+                $resultsOffer = $offerManager->selectOfferByResearchForm($offerInfos);
+            }
+        }
+        $offerInfos = [
+            'product' => $product,
+            'productType' => $productType,
+            'transaction' => $transaction,
+            'department' => $department,
+            'city' => $city
+        ];
+
+        return $this->twig->render('Offer/results.html.twig', [
+            'departments' => $departments,
+            'citiesByDepartment' => $citiesByDepartment,
+            'transactions' => $transactions,
+            'products' => $products,
+            'errors' => $errors,
+            'offerInfos' => $offerInfos,
+            'resultsOffer' => $resultsOffer
+        ]);
     }
 }
