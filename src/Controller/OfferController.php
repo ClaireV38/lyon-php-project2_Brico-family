0bd7054497908e3012322b862e0632ed5f13bb34
Grégory Troupenat <gregory.troupenat@gmail.com>
2020-11-24 16:55:32 +0100

    Search engine form secured in results page - error messages added

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -209,19 +209,42 @@ class OfferController extends AbstractController
         }
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn-index-search']) && !empty($_POST)) {
-            if (isset($_POST['product_type'])) {
-                $productType = $_POST['product_type'];
+            if (!isset($_POST['tools_products']) && !isset($_POST['materials_products'])) {
+                $errors['product'] = 'Veuillez choisir une catégorie de produit';
             }
-            if (isset($_POST['tools_products'])) {
-                $product = $_POST['tools_products'];
+            if (!isset($_POST['product_type'])) {
+                $errors['productType'] = 'Veuillez choisir un type de produit';
+            } else {
+                $productType = $_POST['product_type'];
+                if (isset($_POST['tools_products']) && $productType === 'tool') {
+                    $product = $_POST['tools_products'];
+                } else {
+                    if (isset($_POST['materials_products'])) {
+                        $product = $_POST['materials_products'];
+                    }
+                }
             }
 
-            if (isset($_POST['transaction'])) {
+            if (!isset($_POST['transaction'])) {
+                $errors['transaction'] = 'Veuillez choisir un type de transaction';
+            } else {
                 $transaction = $_POST['transaction'];
             }
 
-            if (isset($_POST['city'])) {
+            if (!isset($_POST['city'])) {
+                $errors['city'] = 'Veuillez choisir une ville';
+            } else {
                 $city = $_POST['city'];
+            }
+
+            if (empty($errors)) {
+                $offerInfos = [
+                    'product' => $product,
+                    'productType' => $productType,
+                    'transaction' => $transaction,
+                    'department' => $department,
+                    'city' => $city
+                ];
             }
         }
 
@@ -235,7 +258,7 @@ class OfferController extends AbstractController
 
         $offerManager = new OfferManager();
         $resultsOffer = $offerManager->selectOfferByResearchForm($offerInfos);
-        if (empty($resultsOffer)) {
+        if (empty($resultsOffer) && !empty($_POST)) {
             $errors['noResult'] = "aucune annonce ne correspond à votre recherche";
         }
 
@@ -281,9 +304,9 @@ class OfferController extends AbstractController
         }
 
         return $this->twig->render('Offer/details.html.twig', [
-        'detailsOffer' => $detailsOffer,
-        'sellerShow' => $sellerShow,
-        'sellerDetails' => $sellerDetails,
-        'images' => $offerImages]);
+            'detailsOffer' => $detailsOffer,
+            'sellerShow' => $sellerShow,
+            'sellerDetails' => $sellerDetails,
+            'images' => $offerImages]);
     }
 }
